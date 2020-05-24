2fdfde95a9f34046cb932614492e66a475c50ec7
kphilemon <kphilemon0529@gmail.com>
2020-05-24 15:39:57 +0800

    Fix base url and location names case sensitivity

--- tripy/widgets/mapview.py
+++ tripy/widgets/mapview.py
@@ -1,21 +1,26 @@
+import os
 from kivy.uix.boxlayout import BoxLayout
 from .cefbrowser import CEFBrowser
 
 
+# Markers are plotted on the eight cities: Kuala Lumpur, Jakarta, Bangkok, Taipei, Hong Kong, Tokyo, Beijing,  Seoul
 class MapView(BoxLayout):
-    # Markers are plotted on the eight cities: Kuala Lumpur, Jakarta, Bangkok, Taipei, Hong Kong, Tokyo, Beijing,  Seoul
-    _BASE_URL = r'file:///assets/map.html'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.browser = CEFBrowser(url=MapView._BASE_URL)
+        if os.getcwd().endswith(r'\tripy\tripy'):
+            self.base_url = r'file:///assets/map.html'
+        else:
+            self.base_url = r'file:///tripy/assets/map.html'
+
+        self.browser = CEFBrowser(url=self.base_url)
         self.add_widget(self.browser)
 
     # plot polyline on the map in ascending order based on the list of location names
-    # valid location names are the eight cities mentioned above
-    # case and space sensitive, invalid location names will be ignored and skip to the next valid location name
+    # valid location names are the eight cities mentioned above (case and space insensitive)
+    # invalid location names will be ignored and skip to the next valid location name
     # to plot a path from Jakarta to Bangkok to Tokyo, simply pass in ['Jakarta', 'Bangkok', 'Tokyo']
     def set_path(self, locations):
-        url = MapView._BASE_URL + '?path=' + ','.join(locations)
+        url = self.base_url + '?path=' + ','.join(locations)
         print('Setting path:', url)
         self.browser.navigate(url)
