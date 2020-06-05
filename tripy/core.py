12db5ac0557670a608201e4454223016a988ffaa
kphilemon <kphilemon0529@gmail.com>
2020-06-05 18:57:18 +0800

    Fix include sentiment analysis

--- tripy/core.py
+++ tripy/core.py
@@ -4,18 +4,29 @@ from typing import List, Dict
 # adjacency_matrix param should contain the distance cost only
 # this function takes in a Dict of analysis and incorporate it into the original distance cost
 # the formula to calculate cost:
-# distance_weightage * distance cost + sentiment_weightage * sentiment cost of destination city
+# distance_weightage * (distance cost / max distance) + sentiment_weightage * sentiment cost of destination city
 def include_sentiment_analysis(adjacency_matrix: List[List[float]], analysis: Dict,
                                distance_weightage: float = 0.5, sentiment_weightage: float = 0.5) -> List[List[float]]:
-
     n = len(adjacency_matrix)
+
+    # make a deep copy and find out the max distance
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    max_distance = 0
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            distance = adjacency_matrix[i][j]
+            if distance > max_distance:
+                max_distance = distance
+
+            matrix[i][j] = matrix[j][i] = distance
 
     # i = from, j = to, adjacency_matrix[i][j] = cost from i to j
     for i in range(n):
         for j in range(n):
             if i != j:
-                distance_cost = adjacency_matrix[i][j]
+                distance_cost = matrix[i][j] / max_distance
                 sentiment_cost = analysis[j]
-                adjacency_matrix[i][j] = distance_weightage * distance_cost + sentiment_weightage * sentiment_cost
+                matrix[i][j] = distance_weightage * distance_cost + sentiment_weightage * sentiment_cost
 
-    return adjacency_matrix
+    return matrix
