58a047d5b125fa2007d0c16dcaab996e8ff37ffc
hj <hongjing1999@gmail.com>
2020-06-21 00:34:43 +0800

    Optimum route added

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,4 +1,87 @@
-from tripy.algorithms.nn import NearestNeighbourSolver
+#from tripy.algorithms.nn import NearestNeighbourSolver
+from typing import List, Dict
+
+class NearestNeighbourSolver:
+    def __init__(self, adjacency_matrix: List[List[float]], sentiment_score: Dict, start: int = 0) -> None:
+        if len(adjacency_matrix) <= 2:
+            raise Exception('Nearest neighbour on 0, 1, or 2 nodes does not make any sense!')
+        if len(adjacency_matrix) != len(adjacency_matrix[0]):
+            raise Exception('Square matrix required for adjacency matrix!')
+        if len(sentiment_score) < len(adjacency_matrix):
+            raise Exception('Insufficient sentiment score!')
+        if not 0 <= start < len(adjacency_matrix):
+            raise IndexError('Starting node must be 0 <= s < N!')
+
+        self._matrix = adjacency_matrix
+        self._scores = sentiment_score
+        self._start = start  # starting node
+        self._solved = False
+        self._N = len(self._matrix)
+        self._cost = 0
+        self._best_route = []
+
+    @property
+    def start(self) -> int:
+        return self._start
+
+    @start.setter
+    def start(self, start: int) -> None:
+        if self._start != start:
+            if not 0 <= start < self._N:
+                raise IndexError('Starting node must be 0 <= s < N!')
+
+            self._start = start
+            self._solved = False  # flag to false so that the algorithm can be execute again
+
+    def cost(self) -> float:
+        self._solve()
+        return self._cost
+
+    def best_route(self) -> List[int]:
+        self._solve()
+        return self._best_route
+
+    def _solve(self) -> None:
+        if self._solved:
+            return
+
+        unvisited = [i for i in range(self._N) if i != self._start]
+        route = [self._start]
+        curr = self._start
+        cost = 0
+
+        while len(unvisited) > 0:
+            distances = [self._matrix[curr][i] for i in unvisited]
+            scores = [self._scores[i] for i in unvisited]
+
+            # sort unvisited nodes by distances & by scores
+            sorted_by_distance = [n for _, n in sorted(zip(distances, unvisited))]
+            sorted_by_score = [n for _, n in sorted(zip(scores, unvisited), reverse=True)]
+
+            # set next to the nearest node
+            _next = sorted_by_distance[0]
+
+            for node in sorted_by_score:
+                # no need to check other nodes with lower score
+                if _next == node:
+                    break
+
+                # go to a further node with better score, which difference of distance is not more than 40%
+                if (self._matrix[curr][node] - self._matrix[curr][_next]) / self._matrix[curr][_next] <= 0.4:
+                    # go only if the difference of score is not less than 2%
+                    if (self._scores[node] - self._scores[_next]) / self._scores[_next] >= 0.02:
+                        _next = node
+                        break
+
+            # visit next node
+            cost += self._matrix[curr][_next]
+            unvisited.remove(_next)
+            route.append(_next)
+            curr = _next
+
+        self._best_route = route
+        self._cost = cost
+        self._solved = True
 
 if __name__ == '__main__':
     adj_matrix = [
--- tripy/app.py
+++ tripy/app.py
@@ -7,18 +7,23 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.dropdown import DropDown
 from kivy.uix.scrollview import ScrollView
+from kivy.config import Config
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from tripy.article.article import SENTIMENT
-from tripy.article.graph import graph, probability_distribution
+from tripy.article.graph import Graph, Probability_distribution, Sentiment_graph
 from tripy.algorithms.nn import NearestNeighbourSolver
 from tripy.algorithms.tsp import DpTspSolver, ModifiedTspSolver
 import tripy.geo.distance as distance
 from tripy.geo.locations import NAME_BY_INDEX, INDEX_BY_NAME
 from tripy.widgets.mapview import MapView
+from threading import Thread
 
 
 class TripyApp(App):
     def __init__(self, **kwargs) -> None:
+        Config.set('graphics','width', 1600)
+        Config.set('graphics','height',900)
+        Config.write()
         super().__init__(**kwargs)
         self.map_view = MapView()
         self.start_button = Button(text="Starting Point", size_hint_y=None, height=40)
@@ -30,13 +35,13 @@ class TripyApp(App):
         self.scroll.add_widget(self.graph_view)
         self.scroll.do_scroll_x=True
         self.scroll.do_scroll_y=False
-        self.button = BoxLayout(orientation='vertical')
         self.scroll1 = ScrollView(size_hint=(None, 1), width=500)
         self.probability_view = BoxLayout(orientation='vertical', size_hint_x=None, width=1000)
-        self.probability_view.bind(minimum_width=self.graph_view.setter('width'))
+        self.probability_view.bind(minimum_width=self.probability_view.setter('width'))
         self.scroll1.add_widget(self.probability_view)
         self.scroll1.do_scroll_x=True
         self.scroll1.do_scroll_y=False
+
 
     def build(self) -> Widget:
         root = BoxLayout(orientation='horizontal')
@@ -48,20 +53,20 @@ class TripyApp(App):
 
         dropdown1 = DropDown()
         for key in NAME_BY_INDEX:
-            btn = Button(text=NAME_BY_INDEX[key], size_hint_y=None, height=40)
+            btn = Button(text=NAME_BY_INDEX[key], size_hint_y=None, height=40, size_hint_x=None, width=500)
             btn.bind(on_release=lambda btn: dropdown1.select(btn.text)) 
             dropdown1.add_widget(btn)
         self.start_button.bind(on_release=dropdown1.open)
         dropdown1.bind(on_select=lambda instance, x: setattr(self.start_button, 'text', x)) 
 
         dropdown2 = DropDown()
-        btn = Button(text="Least Distance travelled", size_hint_y=None, height=40)
+        btn = Button(text="Least Distance travelled", size_hint_y=None, height=40, size_hint_x=None, width=500)
         btn.bind(on_release=lambda btn: dropdown2.select(btn.text)) 
         dropdown2.add_widget(btn)
         btn = Button(text="Better Investment", size_hint_y=None, height=40)
         btn.bind(on_release=lambda btn: dropdown2.select(btn.text)) 
         dropdown2.add_widget(btn)
-        btn = Button(text="Modified TSP", size_hint_y=None, height=40)
+        btn = Button(text="Optimum Route", size_hint_y=None, height=40)
         btn.bind(on_release=lambda btn: dropdown2.select(btn.text)) 
         dropdown2.add_widget(btn)
         
@@ -74,14 +79,18 @@ class TripyApp(App):
         left_top.add_widget(route_button)
 
         dropdown3 = DropDown()
+        btn = Button(text="Sentiment Score", size_hint_y=None, height=40, size_hint_x=None, width=500)
+        btn.bind(on_release=lambda btn: dropdown3.select(btn.text)) 
+        dropdown3.add_widget(btn)
         for key in NAME_BY_INDEX:
-            btn = Button(text=NAME_BY_INDEX[key], size_hint_y=None, height=40)
+            btn = Button(text=NAME_BY_INDEX[key], size_hint_y=None, height=40, size_hint_x=None, width=500)
             btn.bind(on_release=lambda btn: dropdown3.select(btn.text)) 
             dropdown3.add_widget(btn)
         self.country_button.bind(on_release=dropdown3.open)
         dropdown3.bind(on_select=lambda instance, x: setattr(self.country_button, 'text', x)) 
         show_graph_button = Button(text="Show Graph", size_hint_y=None, height=40, size_hint_x=None, width=500, on_press=self._country_on_click)
 
+        
         left_mid.add_widget(self.scroll1)
 
         left_bot.add_widget(self.country_button)
@@ -115,28 +124,34 @@ class TripyApp(App):
             for i in route:
                 path += NAME_BY_INDEX[i] + ","
 
-        elif self.option_button.text == "Modified TSP":
+        elif self.option_button.text == "Optimum Route":
             m = distance.adjacency_matrix()
             scores = SENTIMENT
             solver = ModifiedTspSolver(m, scores, start=INDEX_BY_NAME[self.start_button.text])
             route = solver.best_route()
             routes = solver.all_route()
-            graph = probability_distribution(routes)
+            graph = Probability_distribution(routes)
             self.probability_view.add_widget(FigureCanvasKivyAgg(graph.plot_graph(0.35)))
             print("Routes: ", routes)
             for i in route:
-                path += i+ ","
-
-        print(path)
+                path += i + ","
+            
+            
         self.map_view.set_path(path)
+        print(path)
+        
 
     def _country_on_click(self, instance):
-        graph1= graph(self.country_button.text)
         self.graph_view.clear_widgets()
-        self.graph_view.add_widget(FigureCanvasKivyAgg(graph1.plot_all_graph(0.35)))
+        if self.country_button.text == "Sentiment Score":
+            graph1 = Sentiment_graph()
+            self.graph_view.add_widget(FigureCanvasKivyAgg(graph1.plot_graph(0.35)))
+        else:
+            graph1= Graph(self.country_button.text)
+            self.graph_view.add_widget(FigureCanvasKivyAgg(graph1.plot_all_graph(0.35)))
 
 
 
 if __name__ == '__main__':
     app = TripyApp()
-    app.run()
+    TripyApp().run()
