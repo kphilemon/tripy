644002efdfe5c88f85f84ea50225d6b3ce023780
hj <hongjing1999@gmail.com>
2020-06-22 20:13:26 +0800

    Fixed small mistake

--- tripy/algorithms/tsp.py
+++ tripy/algorithms/tsp.py
@@ -136,7 +136,7 @@ class ModifiedTspSolver(TspSolver):
                 self._tsp(route=route + [nodes[n]], nodes=nodes[:n] + nodes[n + 1:],
                               cost=cost + self._matrix[route[-1]][nodes[n]])
             #if distance between node n and nearest node from current node, and sentiment score different more than 0.02
-            if self._matrix[route[-1]][nodes[n]] - shortest_distance <= shortest_distance * 0.4:
+            elif self._matrix[route[-1]][nodes[n]] - shortest_distance <= shortest_distance * 0.4:
                 if self._score[nodes[n]] - self._score[nearest]  >= self._score[nearest] * 0.02:
                     self._tsp(route=route + [nodes[n]], nodes=nodes[:n] + nodes[n + 1:],
                               cost=cost + self._matrix[route[-1]][nodes[n]])
